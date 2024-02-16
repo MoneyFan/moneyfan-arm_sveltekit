1ec927e2c4dc3c0f0d624a83b436cd5b779464d3
Gerzhan <nikolay.gerzhan@gmail.com>
2024-02-16 17:06:34 +0700

    chore: format code

--- svelte.config.js
+++ svelte.config.js
@@ -3,24 +3,28 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess({})],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'docs',
-			assets: 'docs',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'docs',
+      assets: 'docs',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+    alias: {
+      $lib: './src/lib',
+      $packages: './src/packages'
+    }
+  }
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+import mkcert from 'vite-plugin-mkcert';
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
+
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit(), mkcert(), crossOriginIsolation()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  server: {
+    https: true,
+    proxy: {},
+    port: 5173
+  }
 });
